949400206f72886d18ff60872f80e17f74bea08b
Marcos Talento <talento.marcos@gmail.com>
2020-02-02 03:14:31 -0300

    Fixed pause menu Resume functionality so now the game pauses and unpauses when is needed.

---
+++ Blacksmith Rune Defender/Assets/CPauseMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CPauseMenu : MonoBehaviour
+{
+
+    public CLevelManager _levelManager;
+
+    public void TooglePauseMenu()
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+        _levelManager.SetPause(gameObject.activeSelf);
+    }
+}
